489b7221e4a3d333a7b5525406071a437fb16e42
Rémi Destigny <rdestign@gmail.com>
2016-02-04 00:07:39 +0100

    fixed link issues + get data from fixer API

--- app/models/daily_currency_rate.rb
+++ app/models/daily_currency_rate.rb
@@ -1,9 +1,30 @@
+require "http"
+
 class DailyCurrencyRate < ActiveRecord::Base
 
   validates :day, presence: true
   validates :currency, presence: true
+  validates :rates, presence: true
+
+  serialize :rates, Hash
 
   def self.fetch_by_currency_date(currency, date)
-    {"USD" => 1.5 - rand}
+    daily_currency_rate = DailyCurrencyRate.find_by(currency: currency, day: date)
+    
+    # rates already exists in DB
+    return daily_currency_rate.rates if !daily_currency_rate.nil?
+
+    # fetch rates from API
+    url = "http://api.fixer.io/" + date.strftime("%Y-%m-%d") + "?base=" + currency
+
+    # @TODO: check if JSON.parse can fail / how it fails 
+    fixer_json = JSON.parse HTTP.get(url).to_s
+    rates = fixer_json["rates"]
+
+    daily_currency_rate = DailyCurrencyRate.new(currency: currency, day: date, rates: rates)
+
+    daily_currency_rate.save
+
+    return daily_currency_rate.rates
   end
 end
