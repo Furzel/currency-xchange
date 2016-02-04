2cca58a58a7582bfe4e38062110ceef87019bd07
Rémi Destigny <rdestign@gmail.com>
2016-02-04 01:20:52 +0100

    Visual updates
    CSS and format helpers to make the app less ugly

--- app/helpers/calculations_helper.rb
+++ app/helpers/calculations_helper.rb
@@ -2,4 +2,16 @@ module CalculationsHelper
   def table_data(calculation_id)
     Calculation.find_by(id: calculation_id).table_weekly_rates
   end
+
+  def format_number(value, precision = 4)
+    ActionController::Base.helpers.number_with_precision(value, precision: precision)
+  end
+
+  def format_percentage(value, precision = 4)
+    ActionController::Base.helpers.number_to_percentage(value, precision: precision)
+  end
+
+  def format_currency(value, currency)
+    ActionController::Base.helpers.number_to_currency(value, unit: currency)
+  end
 end
